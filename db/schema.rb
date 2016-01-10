8c464b01238a1e3423c3c59b19a2432f02c3dbea
kaylee42 <kayleeknowles42@gmail.com>
2016-01-10 11:45:26 -0500

    adds sign up with error messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109003830) do
+ActiveRecord::Schema.define(version: 20160110161622) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20160109003830) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "bio"
   end
 
 end
