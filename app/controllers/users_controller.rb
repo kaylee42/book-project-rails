a4a4030807b72a07868ffa46f7cf271f6f1436df
kaylee42 <kayleeknowles42@gmail.com>
2016-01-10 16:15:35 -0500

    adds user delete function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 
   def login
