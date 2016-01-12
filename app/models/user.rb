867c0f23573011ab3ac9fb97b68e33ac77eeeda4
kaylee42 <kayleeknowles42@gmail.com>
2016-01-12 01:22:36 -0500

    adds follower relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   has_many :frienships
   has_many :friends, through: :frienships
 
+  has_many :follows, class_name: "Frienship", foreign_key: "friend_id"
+  has_many :followers, through: :follows, source: :user
+
+
   has_secure_password
   validates_presence_of :name, :username
   validates :username, uniqueness: {case_sensitive: false}
